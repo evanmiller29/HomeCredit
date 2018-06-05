57ed0101452280e15aa4fed5889e0127d171b66b
evanmiller29 <evanmiller29@gmail.com>
2018-06-05 12:14:38 +1200

    Completing feature engineering

--- main_query.py
+++ main_query.py
@@ -11,6 +11,8 @@ import stacknet_funcs as funcs
 from os.path import join
 
 folder = "F:/Nerdy Stuff/Kaggle/Home credit/data"
+stacknet_folder = "C:/Users/Evan/PycharmProjects/StackNet/"
+
 missing_thres = 0.85
 
 print('Importing data...')
@@ -65,24 +67,123 @@ buro['AVERAGE_LOAN_TYPE'] = buro['BUREAU_LOAN_COUNT']/buro['BUREAU_LOAN_TYPES']
 buro['CREDIT_ACTIVE_BINARY'] = buro['CREDIT_ACTIVE'].apply(lambda x: funcs.open_or_closed(x))
 buro['ACTIVE_LOANS_PERCENTAGE_TEST'] = buro.groupby('SK_ID_CURR')['CREDIT_ACTIVE_BINARY'].transform("mean")
 
-print("Converting the DAYS_CREDIT TO A POSITIVE NUMBER")
+# Calculating the difference between credit periods
 
-test = buro.iloc[0:1000, :]
+# Groupby each Customer and Sort values of DAYS_CREDIT in ascending order
+grp = buro[['SK_ID_CURR', 'SK_ID_BUREAU', 'DAYS_CREDIT']].groupby(by = ['SK_ID_CURR'])
+grp1 = grp.apply(lambda x: x.sort_values(['DAYS_CREDIT'], ascending = False)).reset_index(drop = True)#rename(index = str, columns = {'DAYS_CREDIT': 'DAYS_CREDIT_DIFF'})
+print("Grouping and Sorting done")
 
-test['DAYS_CREDIT_TEMP'] = test['DAYS_CREDIT'].apply(lambda x: x * -1)
-test['DAYS_DIFF'] = test.groupby('SK_ID_CURR')['DAYS_CREDIT_TEMP'].apply(lambda x: x.sort_values('DAYS_CREDIT_TEMP', ascending = False)).reset_index(drop = True).diff().fillna(0)
-test['AVG_DAYS_DIFF'] = test.groupby('SK_ID_CURR')['DAYS_CREDIT_TEMP'].transform("mean")
+# Calculate Difference between the number of Days
+grp1['DAYS_CREDIT1'] = grp1['DAYS_CREDIT']*-1
+grp1['DAYS_DIFF'] = grp1.groupby(by = ['SK_ID_CURR'])['DAYS_CREDIT1'].diff()
+grp1['DAYS_DIFF'] = grp1['DAYS_DIFF'].fillna(0).astype('uint32')
+del grp1['DAYS_CREDIT1'], grp1['DAYS_CREDIT'], grp1['SK_ID_CURR']
+gc.collect()
+print("Difference days calculated")
 
-print(test[['SK_ID_CURR', 'SK_ID_BUREAU', 'DAYS_CREDIT', 'DAYS_DIFF', 'AVG_DAYS_DIFF']].head(20))
+buro = buro.merge(grp1, on = ['SK_ID_BUREAU'], how = 'left')
+print("Difference in Dates between Previous CB applications is CALCULATED ")
+print(buro.shape)
 
-test['DAYS_DIFF'] = test[['SK_ID_CURR', 'SK_ID_BUREAU', 'DAYS_CREDIT']].groupby('SK_ID_CURR')['DAYS_CREDIT1'].diff().fillna(0)
+print("% of loans per customer where end date for credit is past")
 
+buro['CREDIT_ENDDATE_BINARY'] = buro.apply(lambda x: funcs.past_end_date(x.DAYS_CREDIT_ENDDATE), axis = 1)
+print("New Binary Column calculated")
 
+grp = buro.groupby(by = ['SK_ID_CURR'])['CREDIT_ENDDATE_BINARY'].mean().reset_index().rename(index=str, columns={'CREDIT_ENDDATE_BINARY': 'CREDIT_ENDDATE_PERCENTAGE'})
+buro = buro.merge(grp, on = ['SK_ID_CURR'], how = 'left')
 
-print(test[['SK_ID_CURR', 'ACTIVE_LOANS_PERCENTAGE']].head(10))
-(test['ACTIVE_LOANS_PERCENTAGE'] - test['ACTIVE_LOANS_PERCENTAGE_TEST']).sum()
+del buro['CREDIT_ENDDATE_BINARY']
 
+print("Average number of days in which credit expires in future")
 
+buro['CREDIT_ENDDATE_BINARY'] = buro['DAYS_CREDIT_ENDDATE']
+
+buro['CREDIT_ENDDATE_BINARY'] = buro.apply(lambda x: funcs.past_end_date(x.DAYS_CREDIT_ENDDATE), axis = 1)
+print("New Binary Column calculated")
+
+# We take only positive values of  ENDDATE since we are looking at Bureau Credit VALID IN FUTURE
+# as of the date of the customer's loan application with Home Credit
+buro_future = buro[buro['CREDIT_ENDDATE_BINARY'] == 1]
+buro_future.shape
+
+print("Calculate Difference in successive future end dates of CREDIT")
+
+print("Create Dummy Column for CREDIT_ENDDATE")
+
+buro_future['DAYS_CREDIT_ENDDATE1'] = buro_future['DAYS_CREDIT_ENDDATE']
+# Groupby Each Customer ID
+grp = buro_future[['SK_ID_CURR', 'SK_ID_BUREAU', 'DAYS_CREDIT_ENDDATE1']].groupby(by = ['SK_ID_CURR'])
+# Sort the values of CREDIT_ENDDATE for each customer ID
+grp1 = grp.apply(lambda x: x.sort_values(['DAYS_CREDIT_ENDDATE1'], ascending = True)).reset_index(drop = True)
+del grp
+gc.collect()
+print("Grouping and Sorting done")
+
+print("Calculate the Difference in ENDDATES and fill missing values with zero")
+grp1['DAYS_ENDDATE_DIFF'] = grp1.groupby(by = ['SK_ID_CURR'])['DAYS_CREDIT_ENDDATE1'].diff()
+grp1['DAYS_ENDDATE_DIFF'] = grp1['DAYS_ENDDATE_DIFF'].fillna(0).astype('uint32')
+del grp1['DAYS_CREDIT_ENDDATE1'], grp1['SK_ID_CURR']
+gc.collect()
+print("Difference days calculated")
+
+print("Merge new feature 'DAYS_ENDDATE_DIFF' with original Data frame for BUREAU DATA")
+
+buro = buro.merge(grp1, on = ['SK_ID_BUREAU'], how = 'left')
+del grp1
+gc.collect()
+
+print("Calculate Average of DAYS_ENDDATE_DIFF")
+
+grp = buro[['SK_ID_CURR', 'DAYS_ENDDATE_DIFF']].groupby(by = ['SK_ID_CURR'])['DAYS_ENDDATE_DIFF'].mean().reset_index().rename( index = str, columns = {'DAYS_ENDDATE_DIFF': 'AVG_ENDDATE_FUTURE'})
+B = buro.merge(grp, on = ['SK_ID_CURR'], how = 'left')
+del grp
+#del B['DAYS_ENDDATE_DIFF']
+del buro['CREDIT_ENDDATE_BINARY'], buro['DAYS_CREDIT_ENDDATE']
+print(buro.shape)
+
+print("Debt over credit ratio")
+
+buro['AMT_CREDIT_SUM_DEBT'] = buro['AMT_CREDIT_SUM_DEBT'].fillna(0)
+buro['AMT_CREDIT_SUM'] = buro['AMT_CREDIT_SUM'].fillna(0)
+
+grp1 = buro[['SK_ID_CURR', 'AMT_CREDIT_SUM_DEBT']].groupby(by = ['SK_ID_CURR'])['AMT_CREDIT_SUM_DEBT'].sum().reset_index().rename( index = str, columns = { 'AMT_CREDIT_SUM_DEBT': 'TOTAL_CUSTOMER_DEBT'})
+grp2 = buro[['SK_ID_CURR', 'AMT_CREDIT_SUM']].groupby(by = ['SK_ID_CURR'])['AMT_CREDIT_SUM'].sum().reset_index().rename( index = str, columns = { 'AMT_CREDIT_SUM': 'TOTAL_CUSTOMER_CREDIT'})
+
+buro = buro.merge(grp1, on = ['SK_ID_CURR'], how = 'left')
+buro = buro.merge(grp2, on = ['SK_ID_CURR'], how = 'left')
+buro['DEBT_CREDIT_RATIO'] = buro['TOTAL_CUSTOMER_DEBT']/buro['TOTAL_CUSTOMER_CREDIT']
+
+del buro['TOTAL_CUSTOMER_DEBT'], buro['TOTAL_CUSTOMER_CREDIT']
+gc.collect()
+print(buro.shape)
+
+print("Overdue debt ratio")
+
+buro['AMT_CREDIT_SUM_DEBT'] = buro['AMT_CREDIT_SUM_DEBT'].fillna(0)
+buro['AMT_CREDIT_SUM_OVERDUE'] = buro['AMT_CREDIT_SUM_OVERDUE'].fillna(0)
+
+grp1 = buro[['SK_ID_CURR', 'AMT_CREDIT_SUM_DEBT']].groupby(by = ['SK_ID_CURR'])['AMT_CREDIT_SUM_DEBT'].sum().reset_index().rename( index = str, columns = { 'AMT_CREDIT_SUM_DEBT': 'TOTAL_CUSTOMER_DEBT'})
+grp2 = buro[['SK_ID_CURR', 'AMT_CREDIT_SUM_OVERDUE']].groupby(by = ['SK_ID_CURR'])['AMT_CREDIT_SUM_OVERDUE'].sum().reset_index().rename( index = str, columns = { 'AMT_CREDIT_SUM_OVERDUE': 'TOTAL_CUSTOMER_OVERDUE'})
+
+buro = buro.merge(grp1, on = ['SK_ID_CURR'], how = 'left')
+buro = buro.merge(grp2, on = ['SK_ID_CURR'], how = 'left')
+del grp1, grp2
+gc.collect()
+
+buro['OVERDUE_DEBT_RATIO'] = buro['TOTAL_CUSTOMER_OVERDUE']/buro['TOTAL_CUSTOMER_DEBT']
+
+del buro['TOTAL_CUSTOMER_OVERDUE'], buro['TOTAL_CUSTOMER_DEBT']
+gc.collect()
+print(buro.shape)
+
+print("Average number of loans prolonged")
+
+buro['CNT_CREDIT_PROLONG'] = buro['CNT_CREDIT_PROLONG'].fillna(0)
+grp = buro[['SK_ID_CURR', 'CNT_CREDIT_PROLONG']].groupby(by = ['SK_ID_CURR'])['CNT_CREDIT_PROLONG'].mean().reset_index().rename( index = str, columns = { 'CNT_CREDIT_PROLONG': 'AVG_CREDITDAYS_PROLONGED'})
+buro = buro.merge(grp, on = ['SK_ID_CURR'], how = 'left')
+print(buro.shape)
 
 #Pre-processing previous_application
 print('Pre-processing previous_application...')
@@ -162,10 +263,8 @@ data = data[data.columns[data.isnull().mean() < missing_thres]]
 del data['SK_ID_CURR']
 del test['SK_ID_CURR']
 
-chdir(folder)
-
-file = "train.txt"
+file = stacknet_folder + "train.txt"
 funcs.from_sparse_to_file(file, data, deli1=" ", deli2=":", ytarget=y)
 
-file = "test.txt"
+file = stacknet_folder + "test.txt"
 funcs.from_sparse_to_file(file, test, deli1=" ", deli2=":", ytarget=None)
